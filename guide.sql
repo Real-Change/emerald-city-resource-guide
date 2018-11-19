a27d2b933e0f9f748aa649d4d6354daf7722f13a
Erin Eckerman <erineckerman@gmail.com>
2018-11-18 16:39:34 -0800

    adding sql file and csv containing current version of the org table

---
+++ guide.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS organization;
+DROP TABLE IF EXISTS organization_x_category;
+DROP TABLE IF EXISTS category;
+
+
+CREATE TABLE IF NOT EXISTS organization ( 
+  organization_id INT, 
+  organization_name VARCHAR(255),
+  website VARCHAR(255),
+  phone_number VARCHAR(1024),
+  org_address VARCHAR(1024),
+  org_description VARCHAR(1024),
+  schedule VARCHAR(1024),
+  gender VARCHAR(64),
+  kids VARCHAR(64),
+  account_id INT
+);
+
+CREATE TABLE IF NOT EXISTS organization_x_category (
+  match_id INT,
+  organization_id INT,
+  category_id INT
+);
+
+CREATE TABLE IF NOT EXISTS category (
+  category_id INT,
+  category_name VARCHAR(255)
+);
+
