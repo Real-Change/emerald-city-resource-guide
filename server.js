65fa368035dc7c2ff21fa968db4275b767af5541
Erin Eckerman <erineckerman@gmail.com>
2019-04-16 21:42:50 -0700

    more firebase config

--- server.js
+++ server.js
@@ -62,29 +62,26 @@ app.post('/login', function(req, res){
   res.render('./pages/auth/login.ejs');
 });
 
-
 // POST method for hardcopy request submission on contact page
 app.post('/confirmation', submitRequest);
 
 // method to submit copy requests
 function submitRequest(req, res){
-  let mailOptions;
+  console.log(req);
+  let mailOptions = {
+    from: req.body.email,
+    to: 'erineckerman@gmail.com',
+    cc: 'erineckerman@gmail.com',
+    subject: '',
+    text: ''
+  };
+
   if (req.body.feedbackfield) {
-    mailOptions = {
-      from: req.body.email,
-      to: 'resourceguide@realchangenews.org',
-      cc: 'erineckerman@gmail.com',
-      subject: 'Feedback on ECRG',
-      text: `${req.body.name} (${req.body.email}) from ${req.body.organization} has submitted the following feedback via the ECRG site: ${req.body.feedbackfield}`
-    }
+    mailOptions.subject = 'Feedback on ECRG';
+    mailOptions.text = `${req.body.name} (${req.body.email}) from ${req.body.organization} has submitted the following feedback via the ECRG site: ${req.body.feedbackfield}`;
   } else {
-    mailOptions = {
-      from: req.body.email,
-      to: 'resourceguide@realchangenews.org',
-      cc: 'erineckerman@gmail.com',
-      subject: 'Request for copies of ECRG',
-      text: `${req.body.name} (${req.body.email}) from ${req.body.organization} has requested ${req.body.number} resource guides. They would like to pick up the guides by ${req.body.date}.`
-    };
+    mailOptions.subject = 'Request for copies of ECRG';
+    mailOptions.text = `${req.body.name} (${req.body.email}) from ${req.body.organization} has requested ${req.body.number} resource guides. They would like to pick up the guides by ${req.body.date}.`;
   }
 
   let transporter = nodemailer.createTransport({
@@ -117,43 +114,63 @@ function submitRequest(req, res){
 // catches
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// method to render results
-function getOrgs (request, response) {
-  let requestType = request.body.submitbutton;
+// method to identify selected categories
+function makeCategoryQuery(category) {
+  // add category selection to SQL query and terminate the query with the last category in the array
+  let categoryQuery = '';
+  category.forEach(function (el) {
+    let i = category.length - 1;
+    if(el === category[i]){
+      categoryQuery = categoryQuery + 'organization_x_category.category_id=' + el;
+    } else {
+      categoryQuery = categoryQuery + 'organization_x_category.category_id=' + el + ' OR ';
+    }
+  });
+  return categoryQuery;
+}
+
+// method to identify selected gender
+function makeGenderQuery(gender){
+  let genderQuery = '';
+
+  // add gender selection to SQL query
+  switch(gender){
+  case 'female':
+    genderQuery = '(gender=\'women only\' OR gender=\'no restrictions\')';
+    break;
+  case 'male':
+    genderQuery = '(gender=\'men only\' OR gender=\'no restrictions\')';
+    break;
+  default:
+    genderQuery = 'gender=\'no restrictions\'';
+  }
+  return genderQuery;
+}
+
+// method to generate SQL query
+function makeSQL (requestType, category, gender){
   let SQL;
   if(requestType === 'all') {
     SQL = 'SELECT DISTINCT organization.*, array_agg(category.category_name) FROM organization INNER JOIN organization_x_category ON organization.organization_id=organization_x_category.organization_id INNER JOIN category ON organization_x_category.category_id=category.category_id GROUP BY organization.organization_id, organization.organization_name, organization.website, organization.phone_number, organization.org_address, organization.org_description, organization.schedule, organization.gender, organization.kids ORDER by organization.organization_name;';
   } else {
-    let {gender, category} = request.body;
     SQL = 'SELECT DISTINCT orgs.*, array_agg(category.category_name) FROM organization AS orgs INNER JOIN organization_x_category ON orgs.organization_id=organization_x_category.organization_id INNER JOIN category ON organization_x_category.category_id=category.category_id WHERE ';
-    let genderQuery = '';
-    let categoryQuery = '';
 
-    // add gender selection to SQL query
-    switch(gender){
-    case 'female':
-      genderQuery = '(gender=\'women only\' OR gender=\'no restrictions\')';
-      break;
-    case 'male':
-      genderQuery = '(gender=\'men only\' OR gender=\'no restrictions\')';
-      break;
-    default:
-      genderQuery = 'gender=\'no restrictions\'';
-    }
-
-    // add category selection to SQL query and terminate the query with the last category in the array
-    category.forEach(el => {
-      let i = category.length - 1;
-      if(el === category[i]){
-        categoryQuery = categoryQuery + 'organization_x_category.category_id=' + el;
-      } else {
-        categoryQuery = categoryQuery + 'organization_x_category.category_id=' + el + ' OR ';
-      }
-    });
+    let genderQuery = makeGenderQuery(gender);
+    let categoryQuery = makeCategoryQuery(category);
 
     // add all the query components  into a single SQL query
     SQL = SQL + genderQuery + ' AND (' + categoryQuery + ') GROUP BY orgs.organization_id, orgs.organization_name, orgs.website, orgs.phone_number, orgs.org_address, orgs.org_description, orgs.schedule, orgs.gender, orgs.kids ORDER by orgs.organization_name;';
   }
+  console.log(SQL);
+  return SQL;
+}
+
+// method to render results
+function getOrgs (request, response) {
+  let requestType = request.body.submitbutton;
+  let {gender, category} = request.body;
+
+  let SQL = makeSQL(requestType, category, gender);
 
   // pass SQL query and values from request to render results
   return client.query(SQL)
@@ -167,3 +184,9 @@ function handleError(err, res) {
   if (res) res.status(500).send('Sorry, something went wrong');
 }
 
+// export methods for testing
+module.exports = {
+  makeCategoryQuery : makeCategoryQuery,
+  makeGenderQuery : makeGenderQuery,
+  makeSQL : makeSQL,
+}
