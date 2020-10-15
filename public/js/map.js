d3119980da58290e2d38321168c4f380fee341bc
Brent Bartlett <brent.bartlett1@gmail.com>
2020-10-15 16:20:01 -0700

    map renders

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
     $('.organization-item').each(function(){
+        if ($(':nth-child(6)', this).text().startsWith('Address')){
+            console.log('Address found!');
+        }
         let mapDiv = document.createElement('div');
         $(mapDiv).css({
             'height': '20vw',
             'width': '30vw',
             'border': '1px solid black'
         });
-        $(this).append(mapDiv);
+        //$(this).append(mapDiv);
         let mymap = L.map(mapDiv).setView([47.620422, -122.349358], 13);
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
             {
