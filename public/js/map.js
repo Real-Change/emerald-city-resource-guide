4151e771af4459dd85cfda3b6a0b7148da15a1ac
Brent Bartlett <brent.bartlett1@gmail.com>
2020-10-15 17:43:22 -0700

    map toggle implemented

--- public/js/map.js
+++ public/js/map.js
@@ -1,16 +1,6 @@
 $(document).ready(function(){
-    $('.organization-item').each(function(){
-        if ($(':nth-child(6)', this).text().startsWith('Address')){
-            console.log('Address found!');
-        }
-        let mapDiv = document.createElement('div');
-        $(mapDiv).css({
-            'height': '20vw',
-            'width': '30vw',
-            'border': '1px solid black'
-        });
-        $(this).append(mapDiv);
-        let mymap = L.map(mapDiv).setView([47.620422, -122.349358], 13);
+    function initMap(container){
+        let myMap = L.map(container).setView([47.620422, -122.349358], 13);
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
             {
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -19,7 +9,39 @@ $(document).ready(function(){
                 tileSize: 512,
                 zoomOffset: -1,
                 accessToken: 'pk.eyJ1IjoidmVyZGhhcm8iLCJhIjoiY2tnNXRzaHE3MHh2ODJxcnVobWNoZjBnaiJ9.lIuTPk6WEWRNbpxLxxXZIA'
-            }).addTo(mymap);
-        let marker = L.marker([47.620422, -122.349358]).addTo(mymap);
+            }).addTo(myMap);
+        let marker = L.marker([47.620422, -122.349358]).addTo(myMap);
+    }
+    $('.organization-item').each(function(){
+        //~ if ($(':nth-child(6)', this).text().startsWith('Address')){
+            //~ console.log('Address found!');
+        //~ }
+        
+        //capture local mapLink (JQ object) and mapDiv (non-JQ object)
+        const mapDiv = this.getElementsByClassName('mapDiv')[0],
+                $mapLink = $('.mapLink', this);
+        
+        //prevents errors from re-initializing the same map
+        let mapExist = false;
+        
+        $(mapDiv).css({
+            'height': '20vw',
+            'width': '30vw',
+            'border': '1px solid black',
+            'display': 'none'
+        });
+        
+        $mapLink.click(function(){
+            $(mapDiv).toggle('slow');
+            
+            //map div must be fully visible to draw into it
+            $(mapDiv).promise().done(function(){
+                if (!mapExist){
+                    initMap(mapDiv);
+                    mapExist = true;
+                }
+            });
+        });
+
     });
 });
