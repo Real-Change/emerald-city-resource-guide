115d742a5a9bcc3af951f715e07a0319614ad81e
Claire <claire_b_jones@hotmail.com>
2021-10-21 11:10:46 -0700

    Revert "minor changes to download requests button"
    This reverts commit ebd15ec3872c888e27e26e807592788f7307a419.

--- server.js
+++ server.js
@@ -895,7 +895,7 @@ app.get("/admin/request/download-requests",
     client
       .query(SQL)
       .then(function(dbQueryResult) {
-          res.send(dbQueryResult.rows);
+          res.status(200).send(dbQueryResult.rows);
       })
       .catch(handleError);
   }
