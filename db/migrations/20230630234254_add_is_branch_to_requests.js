fe572f38326674ed7fc333a2b80c3833224b384e
Mason Malone <651224+MasonM@users.noreply.github.com>
2023-06-30 16:55:08 -0700

    Update contact page for 6th edition orders

---
+++ db/migrations/20230630234254_add_is_branch_to_requests.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('requests', function (table) {
+    table.boolean('is_branch').defaultTo(false);
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.table('requests', function (table) {
+    table.dropColumn('is_branch');
+  })
+};
